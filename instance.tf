e3f2b8c32e9775869030f673b90c1184b8a85bd7
Carlton T Northern <carlton.northern@gmail.com>
2024-05-02 18:09:51 -0700

    Add provisioner to install Docker via bash script

--- instance.tf
+++ instance.tf
@@ -10,3 +10,43 @@ resource "aws_key_pair" "terraform_ec2_key" {
   key_name = "id_ed25519"
   public_key = "${file("~/.ssh/id_ed25519.pub")}"
 }
+
+# an empty resource block
+resource "null_resource" "name" {
+
+  # ssh into the ec2 instance
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_ed25519")
+    host        = aws_instance.pebblepost.public_ip
+  }
+
+  # copy the deployment.sh from your computer to the ec2 instance
+  provisioner "file" {
+    source      = "deployment.sh"
+    destination = "/home/ubuntu/deployment.sh"
+  }
+
+  # set permissions and run the build_docker_image.sh file
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /home/ubuntu/deployment.sh",
+      "sh /home/ubuntu/deployment.sh",
+
+    ]
+  }
+
+  # wait for ec2 to be created
+  depends_on = [aws_instance.pebblepost]
+
+}
+
+# print the IP and hostname of the EC2 instance
+output "instance_ip" {
+  value = join("", ["IP: ", aws_instance.pebblepost.public_ip])
+}
+
+output "instance_dns" {
+  value = join("", ["Hostname: ", aws_instance.pebblepost.public_dns])
+}
